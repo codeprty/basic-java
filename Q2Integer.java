fe6716b94e5676148875531566b793864ca51091
Voon CH <voonch85@gmail.com>
2025-02-19 16:00:10 +0800

    Declare the numX integer variable

--- Q2Integer.java
+++ Q2Integer.java
@@ -1,24 +1,24 @@
 public class Q2Integer {
     public static void main(String[] args) {
-        // Declare an integer variable
-        int my_int;
+        // Declare integer variables
+        int num1 = 2, num2 = 4, num3 = 6;
+        
+        // Declare an integer variable and initialize it to num1
+        int my_int = num1;
 
-        // Initialize my_int to the first value
-        my_int = 2;
-
-        // Prints the value of my_int
+        // Print the value of my_int
         System.out.println("my_int = " + my_int);
 
-        // Reassign my_int to the second value
-        my_int = 4;
+        // Reassign my_int to num2
+        my_int = num2;
 
-        // Prints the updated value of my_int
+        // Print the updated value of my_int
         System.out.println("my_int = " + my_int);
 
-        // Reassign my_int to the third value
-        my_int = 6;
+        // Reassign my_int to num3
+        my_int = num3;
 
-        // Prints the updated value of my_int
+        // Print the updated value of my_int
         System.out.println("my_int = " + my_int);
     }
 }
