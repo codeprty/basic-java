b0320ea18f286583e1de64469f2138de6444ec8b
Voon CH <voonch85@gmail.com>
2025-02-19 15:58:16 +0800

    Remove argument-handling statements

--- Q1Boolean.java
+++ Q1Boolean.java
@@ -9,8 +9,11 @@ public class Q1Boolean {
         // Print the value of my_boolean
         System.out.println("my_boolean = " + my_boolean);
 
+        // Declare a boolean variable and initialize it to false
+        boolean test = false;
+
         // Reassign my_boolean to another boolean value
-        my_boolean = false;
+        my_boolean = test;
 
         // Print the updated value of my_boolean
         System.out.println("my_boolean = " + my_boolean);
