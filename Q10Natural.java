0e0de4a5a03df56233c90e48f609789917bfeadd
Voon CH <voonch85@gmail.com>
2025-02-21 17:06:29 +0800

    Updated

--- Q10Natural.java
+++ Q10Natural.java
@@ -1,14 +1,17 @@
 public class Q10Natural {
     public static void main(String[] args) {
+        // Parse the input argument to an integer
         int n = Integer.parseInt(args[0]);
         int sum = 0;
 
+        // Print the input value
         System.out.println("The first n natural numbers are: " + n);
 
-        // Print numbers in a single loop
+        // Use StringBuilder to store number sequence and sum expression
         StringBuilder numbers = new StringBuilder();
         StringBuilder sumExpression = new StringBuilder();
         
+        // Loop through natural numbers from 1 to n
         for (int i = 1; i <= n; i++) {
             numbers.append(i);
             sumExpression.append(i);
@@ -19,6 +22,7 @@ public class Q10Natural {
             }
         }
 
+        // Print the number sequence, sum expression, and final sum result
         System.out.println(numbers);
         System.out.println(sumExpression + " = " + sum);
         System.out.println("Sum of first n natural numbers are: " + sum);
